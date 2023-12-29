154300f204f234eb6f30b7a7e65f4f5e75f0c5c2
Brunno Carvalho <brunno.ddcbjj@gmail.com>
2023-12-29 18:38:52 -0300

    Desafio Consulta Vendas Concluido

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,22 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
-
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SaleSummaryProjection;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.devsuperior.dsmeta.dto.SaleMinDTO;
-import com.devsuperior.dsmeta.entities.Sale;
-import com.devsuperior.dsmeta.repositories.SaleRepository;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SaleService {
@@ -19,5 +28,23 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> findReport(String minDate, String maxDate, String sellerName, Pageable pageable) {
+
+		LocalDate maxLocalDate = maxDate.equals("") ? LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()) : LocalDate.parse(maxDate);
+		LocalDate minLocalDate = minDate.equals("") ? maxLocalDate.minusYears(1L) : LocalDate.parse(minDate);
+
+		return repository.searchReport(minLocalDate, maxLocalDate, sellerName, pageable);
+
+	}
+
+	public List<SaleSummaryDTO> findSummary(String minDate, String maxDate) {
+
+		LocalDate maxLocalDate = maxDate.equals("") ? LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()) : LocalDate.parse(maxDate);
+		LocalDate minLocalDate = minDate.equals("") ? maxLocalDate.minusYears(1L) : LocalDate.parse(minDate);
+
+		List<SaleSummaryProjection> result = repository.searchSummary(minLocalDate, maxLocalDate);
+		return result.stream().map(x -> new SaleSummaryDTO(x)).collect(Collectors.toList());
 	}
 }
